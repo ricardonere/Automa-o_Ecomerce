271dcf09fb8e4f58829275566c9244474bc350c9
Ricardo <ricardonere@gmail.com>
2021-07-11 12:27:00 -0300

    criando o Primeiro Teste Amazon

--- src/test/java/page/BuscaPage.java
+++ src/test/java/page/BuscaPage.java
@@ -1,5 +1,48 @@
 package page;
 
-public class BuscaPage {
+import java.util.concurrent.TimeUnit;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BuscaPage {
+	static ChromeDriver driver;
+//	FormularioPage formularioPage = new FormularioPage(driver);
+
+	@BeforeClass
+	public static void beforeClass() {
+		driver = setupChromeDriver();
+		driver.get("http://amazon.com.br");// Abrindo uma pagina
+
+	}
+	@Test
+	public void testeBuscaAmazon () {
+		
+		
+		
+	}
+	
+	
+	public static ChromeDriver setupChromeDriver() {
+		
+		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		
+		driver = new ChromeDriver(chromeOptions);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+//		"--disable-web-security", "--ignore-certificate-errors", "--allow-running-insecure-content", "--allow-insecure-localhost", "--disable-gpu", "window-size=1200x600", "disable-popup-blocking", "disable-infobars"
+//		);
+		
+		return driver;
+		
+	}
+	
+	
+	
+	
+	
+	
 }
