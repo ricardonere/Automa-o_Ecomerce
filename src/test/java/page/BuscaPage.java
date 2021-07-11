d354e5c3895e5d4f3dbde083e61109f962b75f45
Ricardo <ricardonere@gmail.com>
2021-07-11 13:04:25 -0300

    Realizando busca na Anazon

--- src/test/java/page/BuscaPage.java
+++ src/test/java/page/BuscaPage.java
@@ -4,45 +4,27 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BuscaPage {
-	static ChromeDriver driver;
-//	FormularioPage formularioPage = new FormularioPage(driver);
+	public ChromeDriver driver;
 
-	@BeforeClass
-	public static void beforeClass() {
-		driver = setupChromeDriver();
-		driver.get("http://amazon.com.br");// Abrindo uma pagina
-
-	}
-	@Test
-	public void testeBuscaAmazon () {
-		
-		
+	public final String BUSCA = "//input[@id='twotabsearchtextbox']";
+	
+	public void preecheBusca() {
+		driver.findElement(By.xpath(BUSCA)).sendKeys("ecodoth alexa 4 geração"); // Localizando
+		driver.findElement(By.xpath(BUSCA)).submit();
 		
 	}
 	
-	
-	public static ChromeDriver setupChromeDriver() {
-		
-		
-		ChromeOptions chromeOptions = new ChromeOptions();
-		
-		driver = new ChromeDriver(chromeOptions);
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-//		"--disable-web-security", "--ignore-certificate-errors", "--allow-running-insecure-content", "--allow-insecure-localhost", "--disable-gpu", "window-size=1200x600", "disable-popup-blocking", "disable-infobars"
-//		);
-		
-		return driver;
-		
+	/**
+	 * Metodo Contrutor
+	 * @param outroDriver
+	 */
+	public BuscaPage(ChromeDriver outroDriver) {
+		this.driver = outroDriver;
 	}
-	
-	
-	
-	
-	
 	
 }
